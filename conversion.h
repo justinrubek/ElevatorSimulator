08165dd893147c7ea53fc1ff85099507302a8a5d
justinrubek <rubejus@bvu.edu>
2017-05-24 21:54:02 -0500

    Removed unneeded curly braces

--- conversion.h
+++ conversion.h
@@ -17,9 +17,7 @@ std::vector<std::string> split(const std::string &s, char delim)
   std::string item;
 
   while (std::getline(ss, item, delim))
-  {
     items.push_back(item);
-  }
 
   return items;
 }
